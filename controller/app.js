e0479c025f5112928b97700069b65222127be723
Thiago Oliveira <onthiagooliveira@outook.com>
2024-09-10 04:58:04 -0300

    second commit into version 1.4
    Imported jquery and starting js file

---
+++ controller/app.js
@@ -0,0 +1,6 @@
+// Show a element that is hidden with in CSS when one button is clicked.
+var hiddenBox = $("#banner-information");
+var hiddenCard = $('#art-explanation')
+hiddenBox.on('click', function ( event ) {
+  windows.
+})
