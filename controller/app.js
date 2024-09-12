a9961a2ef8c9cb1cf39fc0440ae1945623a08235
Thiago Oliveira <onthiagooliveira@outook.com>
2024-09-11 22:10:56 -0300

    created interactions on buttons com jQuery

--- controller/app.js
+++ controller/app.js
@@ -1,10 +1,12 @@
 // Show a element that is hidden with in CSS when one button is clicked.
-let openModal = $("#modal-art");
-let favDialog = $('#favDialog');
-let hiddenArtInforation = $('#home-cards-info-art');
+let showModal = $("#modal-art");
+let closeModal = $(".close-icon");
+let favDialog = $("#favDialog");
+let cardQuestion = $("#home-cards-info-art");
 
-openModal.on("click", function (event) {
-  favDialog.show();
-  hiddenArtInforation.hide()
-  
+// Quando o botão de mostrar é clicado
+showModal.on("click", function () {
+  favDialog.slideToggle('fast'); // Mostra o favDialog
+  console.log(`A caixa de pergunta está inativa`);
 });
+
